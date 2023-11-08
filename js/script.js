7be4a8025d71ffe7080323ce14b9717e72fa6389
Vadim Faust <vadim40328@gmail.com>
2023-11-08 16:43:50 +0700

    WIP slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,78 @@
-$('.__initial').removeClass('__initial')
+function MyTransition( Splide, Components ) {
+  const { Move } = Components;
+  const { list } = Components.Elements;
+
+  let endCallback;
+
+  function start( index, done ) {
+    // Converts the index to the position
+    const destination = Move.toPosition( index, true );
+
+    // Applies the CSS transition
+    list.style.transition = 'transform 1000ms cubic-bezier(.44,.65,.07,1.01)';
+
+    // Moves the carousel to the destination.
+    Move.translate( destination );
+
+    // Keeps the callback to invoke later.
+    endCallback = done;
+  }
+
+  function cancel() {
+    list.style.transition = '';
+  }
+
+  return {
+    start,
+    cancel,
+  };
+}
+
+let main_pics = new Splide('.main_pics', {
+  focus: 'center',
+  type: 'loop',
+  perPage: 3,
+  arrows: false,
+  gap: '50%',
+  pagination: false,
+  updateOnMove: true,
+  autoplay: true,
+  pauseOnHover: false,
+  interval: 4000,
+});
+main_pics.mount({}, MyTransition);
+
+const desktop_width = 1650;
+
+async function animate_top() {
+  $('.top_banner .content').addClass('__clipped');
+  await new Promise(r => setTimeout(r, 1000));
+  $('.top_banner .content').hide();
+  $('.main_pics').fadeIn();
+  main_pics.refresh();
+}
+
+async function animate_buttons() {
+  $('.bottom_blocks .block').removeClass('__initial');
+}
+
+async function animate_about() {
+  $('.about_box').removeClass('__initial');
+  await new Promise(r => setTimeout(r, 1000));
+  if (window.innerWidth > desktop_width) {
+    $('.about_box .content').removeClass('__initial');
+    $('.about_box .shade').removeClass('__initial');
+  } else {
+    $('.about_box .content').slideDown();
+    await new Promise(r => setTimeout(r, 500));
+    $('.about_box .shade').removeClass('__initial');
+  }
+}
+
+let product_pics = new Splide('.product_slider', {
+  perPage: 3,
+  pagination: false,
+  gap: 24,
+  // padding: 106,
+});
+product_pics.mount({}, MyTransition);
